05c413bb2eb9c8a38ccec9ea22ae753a94e9bdec
bestow-jd <jd@bestow.com>
2023-08-23 17:13:17 -0500

    add percy back in

--- playwright/visual-regression.spec.ts
+++ playwright/visual-regression.spec.ts
@@ -4,5 +4,5 @@ import { test } from "@playwright/test";
 test(`BLIC: Homepage Snapshot @dev @qa @vRegression`, async ({ page }) => {
   await page.goto("http://localhost:3000", { waitUntil: "networkidle" });
   await page.waitForURL("http://localhost:3000");
-  //   await percySnapshot(page, `Home Page`);
+    await percySnapshot(page, `Home Page`);
 });
